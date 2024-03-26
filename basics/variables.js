9f52f98fdb2dfa3c9d67a29d8a5d52930b281cc7
Ashutosh724425 <153819023+Ashutosh724425@users.noreply.github.com>
2024-03-26 16:05:27 +0000

    updated

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const name = "Ashutosh"
+let address = "lucknow"
+var mobile = "1335346544"
+state = "uttar pradesh"
+let country
+
+// trying to modify the values
+
+// name = ashu
+// console.log(name);
+
+address = "ambedkarnagar"
+mobile = "888888888888"
+state = "up"
+
+console.log(address);
+console.table([address, mobile, state, country])
